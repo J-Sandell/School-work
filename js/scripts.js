d4d8a756e9dab9c7abed8927a4914369c719ae72
Jeff Sandell <sandelljeffrey88@gmail.com>
2021-10-11 19:47:27 -0700

    NPM scripts Part 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function() {
-        if($("#carouselButton").children("i").hasClass("fa-pause")) {
+$(function () {
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
             $("#carouselButton").children("i").addClass("fa-play");
